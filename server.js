6f1e8ab179144fc14e45bcdae58ff31f501d692c
salonichavande-26 <saloni.chavande.cmaug25@gmail.com>
2025-10-31 16:42:45 +0530

    file change update

--- server.js
+++ server.js
@@ -10,5 +10,12 @@ const userSchema = new mangoose.Schema({
 
 userSchema.pre("save",async function(next){
  if(!this.isModified("password")) return next();
- const salt = await
-})
+ const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password, salt);
+ next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+Module.exports = mangoose.model("user", userSchema);
